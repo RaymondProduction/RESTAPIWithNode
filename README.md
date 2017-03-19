31ffe1fe8883a23fb3f73117da5d45c3f8710acb
raymondproduction <raymondproduction@narod.ru>
2017-03-20 00:14:01 +0200

    Added routers and controllers for REST

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Create a REST API With Node.js
 
-###1. Step
+### 1. Step
 Simple server
-###2. Step
+### 2. Step
 Use Express
+### 3. Step
+REST API, (Routers, request, response, controllers)
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
-app.get('/', function(req, res) {
-  res.send('Hello Seattle\n');
-});
+var router = require('./routes');
+router(app);
 app.listen(3001);
 console.log('Listening on port 3001...');
