a48e038280f67720598cd9130a9fce6e6798dbbb
raymondproduction <raymondproduction@narod.ru>
2017-03-19 23:37:55 +0200

    Use Express

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ###1. Step
 Simple server
+###2. Step
+Use Express
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello New York\n');
-}).listen(3001);
-console.log('Server running at http://localhost:3001/');
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+  res.send('Hello Seattle\n');
+});
+app.listen(3001);
+console.log('Listening on port 3001...');
